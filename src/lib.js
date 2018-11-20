ea12500dfe7ab21c9c09100076b722f8495ec548
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-11-20 12:46:05 +0530

    intial commit for the array functions
    created the src directory where all the functions in the library file
    created teh test directory where all the test functions will be there

---
+++ src/lib.js
@@ -0,0 +1,6 @@
+const map = function(array,func) {
+  let resultArray = [];
+  return array;
+}
+
+module.exports = { map };
---
+++ test/functionTests.js
@@ -0,0 +1,4 @@
+const assert = require('assert');
+const {map} = require('../src/lib.js');
+
+assert.deepEqual(map([1,2,3,]),[1,2,3]);
